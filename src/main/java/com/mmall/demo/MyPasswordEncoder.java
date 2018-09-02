de677ed8a5569de5ed44688a47be83799c2c150e
Elvis.Chen <chenyihongyi@163.com>
2018-09-02 22:28:43 +0800

    初始化
    Signed-off-by: Elvis.Chen <chenyihongyi@163.com>

---
+++ src/main/java/com/mmall/demo/MyPasswordEncoder.java
@@ -0,0 +1,18 @@
+package com.mmall.demo;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class MyPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence charSequence) {
+
+
+        return null;
+    }
+
+    @Override
+    public boolean matches(CharSequence charSequence, String s) {
+        return false;
+    }
+}
